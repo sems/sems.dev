3ca4b1d2c6b4e6ee81823f867b505c3abb95dd5d
Sem Spanhaak <semspanhaak@gmail.com>
2018-06-06 13:31:21 +0200

    Alternative way for outer images

--- home-app/src/components/case.js
+++ home-app/src/components/case.js
@@ -7,25 +7,24 @@ export class Case extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {	
-
+      backgroundStyles: this.getOuter(),
 		};
 	}
 	
 	getOuter = () => {
-		const imageURI = "../assets/images/comp/" + this.props.title + "_comp.jpg";
+		const imageURI = require("../assets/images/comp/" + this.props.title + "_comp.jpg");
+		
 		const styles = { 
-			backgroundImage: "url(" + imageURI +")",
-			height: 250, 
-			width: 250
+			backgroundImage: "url('" + imageURI +"')",
 		}
 		return styles;
 	}
 
 	render() {
 		return(
-			<Tilt className={this.props.title + ' Tilt'} options={{ max : 25 }} style={ this.getOuter() } >
+			<Tilt className={this.props.title + ' Tilt'} options={{ max : 25 }} style={ this.state.backgroundStyles } >
 				<a href={this.props.externalLink}>
-					<img className="Tilt-inner svg" src={require("../assets/images/" + this.props.inner)} alt={this.props.title} />
+					<img className="Tilt-inner" src={require("../assets/images/" + this.props.inner)} alt={this.props.title} />
 				</a>
 			</Tilt>
 		)
