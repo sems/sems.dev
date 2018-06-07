06650cfb18c16f4c554f7148a18136ffbd57b462
Sem Spanhaak <semspanhaak@gmail.com>
2018-06-07 10:06:07 +0200

    Improved expand

--- home-app/src/components/case.js
+++ home-app/src/components/case.js
@@ -33,21 +33,25 @@ export class Case extends React.Component {
     let case_class = this.state.expanded ? "preview_expanded" : "preview";
 		return(
       <div>
-        <Tilt className={this.props.title + ' Tilt ' + case_class} options={{ max : 25 }} style={ !this.state.expanded && this.state.styles } >
-          <a onClick={this.expandClickHandler}>
-            <img className="Tilt-inner" src={this.state.expanded ? this.state.innerIconExpanded : this.state.innerIcon} alt={this.props.title} />
-          </a>
-          {this.state.expanded && 
-          <div className="inner-expanded">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-              Id consequatur aliquid tempora dicta neque, sequi necessitatibus nobis. 
-              Dolores, magnam excepturi! Ullam, quae. Maiores earum aperiam alias vel? 
-              Rem, facilis a.
-            </p>
-            <a href={this.props.externalLink}>Naar site</a>
-          </div>}
-        </Tilt>
+          <Tilt className={this.props.title + ' Tilt ' + case_class} options={{ max : 23, scale: '1.05' }} style={ !this.state.expanded && this.state.styles } >
+            <a className="case_link" onClick={this.expandClickHandler}>
+              <img className="Tilt-inner" src={this.state.expanded ? this.state.innerIconExpanded : this.state.innerIcon} alt={this.props.title} />
+            </a>
+            {this.state.expanded && 
+            <div className="inner-expanded">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Id consequatur aliquid tempora dicta neque, sequi necessitatibus nobis. 
+                Dolores, magnam excepturi! Ullam, quae. Maiores earum aperiam alias vel? 
+                Rem, facilis a.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Id consequatur aliquid tempora dicta neque, sequi necessitatibus nobis. 
+                Dolores, magnam excepturi! Ullam, quae. Maiores earum aperiam alias vel? 
+                Rem, facilis a.
+              </p>
+              <a href={this.props.externalLink}>To site</a>
+            </div>}
+          </Tilt>
       </div>
 		)
 	}
