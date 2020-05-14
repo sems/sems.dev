9d6fffd778141adaa0c58f29854a3f182a20b293
Sem <me@sems.dev>
2020-05-14 23:40:43 +0200

    Update header.js (#26)
    * Update header.js
    
    Generic links to own domain links
    
    * Update header.js
    
    Link to GH changed

--- app/src/components/header.js
+++ app/src/components/header.js
@@ -19,13 +19,13 @@ export class Header extends React.Component {
 						<span role="img" aria-label="waving hand">👋</span> Hi, nice meeting you!
 					</p>
 					<p>
-						My name is <span className="me">Sem Spanhaak</span>, currently studying for my BSc in Software Engineering at <a className="windes" href="//www.windesheim.com/">Windesheim</a>. Besides school, I work as a copywriter, sales assistant, and product specialist at <a className="azerty" href="//azerty.nl/">Azerty</a>. I did some internships at <a className="akyla" href="//akyla.nl">Akyla</a>, <a className="iq" href="//iqmedia.nl">IQ-Media</a>, and <a className="egm" href="//epicode.nl">Epicode</a>. People I've worked for are <a className="creative-orange" href="//creativeorange.nl">Creative Orange</a>, <a className="e-active" href="//e-active.nl/">E-Active</a>, <a className="landstede" href="//landstedembo.nl">Landstede</a>, <a className="azerty" href="//azerty.nl/">Azerty</a>, and <a className="dielle" href="//www.diellepelletkachel.nl/">Dielle</a>.
+						My name is <span className="me">Sem Spanhaak</span>, currently studying for my BSc in Software Engineering at <a className="windes" href="//link.sems.dev/school">Windesheim</a>. Besides school, I work as a copywriter, sales assistant, and product specialist at <a className="azerty" href="//link.sems.dev/azerty/">Azerty</a>. I did some internships at <a className="akyla" href="//link.sems.dev/akyla">Akyla</a>, <a className="iq" href="//link.sems.dev/iq">IQ-Media</a>, and <a className="egm" href="//link.sems.dev/epi">Epicode</a>. People I've worked for are <a className="creative-orange" href="//link.sems.dev/orange">Creative Orange</a>, <a className="e-active" href="//link.sems.dev/active">E-Active</a>, <a className="landstede" href="//link.sems.dev/landstede">Landstede</a>, <a className="azerty" href="//link.sems.dev/azerty/">Azerty</a>, and <a className="dielle" href="//link.sems.dev/dielle">Dielle</a>.
 					</p>
 					<p>
 						I like to explore the world, talk about games, technology, and so much more.
 					</p>
 					<p>
-						If you would like to contact me, you can! Try trough <a className="twitter" href="//twitter.com/semspanhaak">Twitter</a>, <a className="linkedin" href="//linkedin.com/in/semspanhaak">LinkedIn</a>, <a className="github" href="//github.com/semspanhaak">Github</a>, or email <a className="me" href="mailto:me@sems.dev">me@sems.dev</a>.
+						If you would like to contact me, you can! Try trough <a className="twitter" href="//link.sems.dev/twitter">Twitter</a>, <a className="linkedin" href="//link.sems.dev/linkedin">LinkedIn</a>, <a className="github" href="//link.sems.dev/gh">Github</a>, or email <a className="me" href="mailto:me@sems.dev">me@sems.dev</a>.
 					</p>
 				</div>
 			</div>
