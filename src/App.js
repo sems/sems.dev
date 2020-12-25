fb6e15c66d5878354b7c86edfc1efcc2ab16ed24
Sem <me@sems.dev>
2020-12-25 18:16:41 +0100

    Update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './styles/page.sass';
+import './styles/header.sass';
+
+import { Header } from './components/header';
+class App extends Component {
+
+  render() {
+    return (
+      <Header />
+    );
+  }
+}
+export default App;
