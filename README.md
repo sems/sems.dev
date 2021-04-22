a1d4d86887ae9393b952d26000646b42b78810a6
Sem <me@sems.dev>
2021-04-22 17:15:42 +0200

    Remove travis.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-[![Build Status](https://travis-ci.org/semspanhaak/sems.dev.svg?branch=master)](https://travis-ci.org/semspanhaak/sems.dev)
 ![GitHub deployments](https://img.shields.io/github/deployments/semspanhaak/sems.dev/github-pages?label=deployment)
 ![LGTM Grade](https://img.shields.io/lgtm/grade/javascript/github/semspanhaak/sems.dev)
 ![LGTM Alerts](https://img.shields.io/lgtm/alerts/github/semspanhaak/sems.dev)
 # sems.dev
+
+## Deployment
+
+Run `npm run deploy` to deploy to live.
--- src/components/header.js
+++ src/components/header.js
@@ -16,7 +16,7 @@ export class Header extends Component {
             I completed internships at <a className="zon" href="//link.sems.dev/zon">Zonneplan</a>, <a className="akyla" href="//link.sems.dev/akyla">Akyla</a>, <a className="iq" href="//link.sems.dev/iq">IQ-Media</a>, and <a className="egm" href="//link.sems.dev/epi">Epicode</a>. People I've worked for previously are <a className="creative-orange" href="//link.sems.dev/orange">Creative Orange</a>, <a className="e-active" href="//link.sems.dev/active">E-Active</a>, <a className="landstede" href="//link.sems.dev/landstede">Landstede</a>, <a className="azerty" href="//link.sems.dev/azerty/">Azerty</a>, and <a className="dielle" href="//link.sems.dev/dielle">Dielle</a>.
 					</p>
           <p>
-            I like to explore the world, take <a className="me" href="//unsplash.com/@semspam">pictures</a> while doing it. Also love to talk about games & technology, and so much more.
+            I like to explore the world, take <a className="me" href="//bysem.me">pictures</a> while doing it. Also love to talk about games & technology, and so much more.
 					</p>
           <p>
             If you would like to contact me, you can! Try through <a className="twitter" href="//link.sems.dev/twitter">Twitter</a>, <a className="linkedin" href="//link.sems.dev/linkedin">LinkedIn</a>, <a className="github" href="//github.com/sems">Github</a>, or email <a className="me" href="mailto:me@sems.dev">me@sems.dev</a>.
