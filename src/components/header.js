8e19208e371a3085eb8c24f911ed0052243dc626
Sem Spanhaak <me@sems.dev>
2021-06-16 23:50:23 +0200

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -10,16 +10,16 @@ export class Header extends Component {
         <div className="main">
           <h1><span role="img" aria-label="waving hand">👋</span> Hi, nice meeting you!</h1>
           <p>
-            My name is <span className="me">Sem Spanhaak</span>, currently studying at <a className="lnu" href="https://lnu.se/en">Linnaeus University</a> as part of my BSc in Software Engineering at <a className="windes" href="//link.sems.dev/school">Windesheim</a>. Besides school, I work as a mobile app developer at <a className="akyla" href="//link.sems.dev/akyla">Akyla</a>. 
+            My name is <span className="me">Sem Spanhaak</span>, currently studying at <a className="lnu" href="//lnu.se/en">Linnaeus University</a> as part of my BSc in Software Engineering at <a className="windes" href="//windesheim.nl/">Windesheim</a>. Besides school, I work as a mobile app developer at <a className="akyla" href="//akyla.nl/">Akyla</a>. 
           </p> 
           <p>
-            I completed internships at <a className="zon" href="//link.sems.dev/zon">Zonneplan</a>, <a className="akyla" href="//link.sems.dev/akyla">Akyla</a>, <a className="iq" href="//link.sems.dev/iq">IQ-Media</a>, and <a className="egm" href="//link.sems.dev/epi">Epicode</a>. People I've worked for previously are <a className="creative-orange" href="//link.sems.dev/orange">Creative Orange</a>, <a className="e-active" href="//link.sems.dev/active">E-Active</a>, <a className="landstede" href="//link.sems.dev/landstede">Landstede</a>, <a className="azerty" href="//link.sems.dev/azerty/">Azerty</a>, and <a className="dielle" href="//link.sems.dev/dielle">Dielle</a>.
+            I completed internships at <a className="zon" href="//zonneplan.nl/">Zonneplan</a>, <a className="akyla" href="//akyla.nl">Akyla</a>, <a className="iq" href="//iqmedia.nl/">IQ-Media</a>, and <a className="egm" href="//epicode.nl/">Epicode</a>. People I've worked for previously are <a className="creative-orange" href="//creativeorange.nl/">Creative Orange</a>, <a className="e-active" href="//e-active.nl/">E-Active</a>, <a className="landstede" href="//landstedembo.nl/">Landstede</a>, <a className="azerty" href="//azerty.nl/">Azerty</a>, and <a className="dielle" href="//diellepelletkachel.nl/">Dielle</a>.
 					</p>
           <p>
             I like to explore the world, take <a className="me" href="//bysem.me">pictures</a> while doing it. Also love to talk about games & technology, and so much more.
 					</p>
           <p>
-            If you would like to contact me, you can! Try through <a className="twitter" href="//link.sems.dev/twitter">Twitter</a>, <a className="linkedin" href="//link.sems.dev/linkedin">LinkedIn</a>, <a className="github" href="//github.com/sems">Github</a>, or email <a className="me" href="mailto:me@sems.dev">me@sems.dev</a>.
+            If you would like to contact me, you can! Try through <a className="twitter" href="//twitter.com/semspanhaak">Twitter</a>, <a className="linkedin" href="//linkedin.com/in/semspanhaak/">LinkedIn</a>, <a className="github" href="//github.com/sems">Github</a>, or email <a className="me" href="mailto:me@sems.dev">me@sems.dev</a>.
           </p>
           <p>
             Getting a message from me, and want to verify if it is really me? Check my public <a download className="gpg key" href="/publickey.me@sems.dev.asc">GPG</a> and <a download className="ssh key" href="/sems.main.pub">SSH</a> keys <span role="img" aria-label="key">🔑</span>.
