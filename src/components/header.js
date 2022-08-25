f02fed766e694ca40f3a6e046b90dfc51f8c6bc5
Sem <me@sems.dev>
2022-08-25 16:58:43 +0200

    Update

--- src/components/header.js
+++ src/components/header.js
@@ -10,10 +10,13 @@ export class Header extends Component {
         <div className="main">
           <h1><span role="img" aria-label="waving hand">👋</span> Hi, nice meeting you!</h1>
           <p>
-            My name is <span className="me">Sem Spanhaak</span>, currently writing my thesis at <a className="wehkamp" href="//wehkamp.nl/">Wehkamp</a>, as part of my bachelor in Software Engineering at <a className="windes" href="//windesheim.nl/">Windesheim</a>. I completed a pre-master in Computing Science at <a className="uu" href="//uu.nl/en">Utrecht University</a> and did part of my bachelor at <a className="lnu" href="//lnu.se/en">Linnaeus University</a>. Besides school, I work as a mobile app engineer at <a className="akyla" href="//akyla.nl/">Akyla</a>. 
+            My name is <span className="me">Sem Spanhaak</span>, soon starting with my masters in Computing Science at <a className="uu" href="//uu.nl/en">Utrecht University</a>. Besides university, I work as an iOS engineer at <a className="wehkamp" href="//wehkamp.nl/">Wehkamp</a> &  <a className="akyla" href="//akyla.nl/">Akyla</a>. 
           </p> 
           <p>
-            I completed internships at <a className="zon" href="//zonneplan.nl/">Zonneplan</a>, <a className="akyla" href="//akyla.nl">Akyla</a>, <a className="iq" href="//iqmedia.nl/">IQ-Media</a>, and <a className="egm" href="//epicode.nl/">Epicode</a>. People I've worked for previously are <a className="creative-orange" href="//creativeorange.nl/">Creative Orange</a>, <a className="e-active" href="//e-active.nl/">E-Active</a>, <a className="landstede" href="//landstedembo.nl/">Landstede</a>, <a className="azerty" href="//azerty.nl/">Azerty</a>, and <a className="dielle" href="//diellepelletkachel.nl/">Dielle</a>.
+            I completed my bachelor in Software Engineering at <a className="windes" href="//windesheim.nl/">Windesheim</a>, pre-master in Computing Science at <a className="uu" href="//uu.nl/en">Utrecht University</a>, and did part of my bachelor at <a className="lnu" href="//lnu.se/en">Linnaeus University</a> in Sweden. 
+          </p>
+          <p>
+            I completed internships at <a className="wehkamp" href="//wehkamp.nl/">Wehkamp</a>, <a className="zon" href="//zonneplan.nl/">Zonneplan</a>, <a className="akyla" href="//akyla.nl">Akyla</a>, <a className="iq" href="//iqmedia.nl/">IQ-Media</a>, and <a className="egm" href="//epicode.nl/">Epicode</a>. People I've worked for previously are <a className="creative-orange" href="//creativeorange.nl/">Creative Orange</a>, <a className="e-active" href="//e-active.nl/">E-Active</a>, <a className="landstede" href="//landstedembo.nl/">Landstede</a>, <a className="azerty" href="//azerty.nl/">Azerty</a>, and <a className="dielle" href="//diellepelletkachel.nl/">Dielle</a>.
 					</p>
           <p>
             I like to explore the world, take <a className="me" href="//bysem.me">pictures</a> while doing it. Also love to talk about games & technology, and so much more.
